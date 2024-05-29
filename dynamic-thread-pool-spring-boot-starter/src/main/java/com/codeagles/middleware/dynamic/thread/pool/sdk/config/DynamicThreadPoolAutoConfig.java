348e2d533bf220d0c34e18b3502275af4f51e588
codeagles <hcn0521@outlook.com>
2024-05-29 22:55:02 +0800

    feat:测试工程

--- dynamic-thread-pool-spring-boot-starter/src/main/java/com/codeagles/middleware/dynamic/thread/pool/sdk/config/DynamicThreadPoolAutoConfig.java
+++ dynamic-thread-pool-spring-boot-starter/src/main/java/com/codeagles/middleware/dynamic/thread/pool/sdk/config/DynamicThreadPoolAutoConfig.java
@@ -1,13 +1,38 @@
 package com.codeagles.middleware.dynamic.thread.pool.sdk.config;
 
+
+import com.alibaba.fastjson2.JSON;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+import java.util.concurrent.ThreadPoolExecutor;
+
 /**
  * @author codeagles
  * @date 2024/5/21
  * @description 动态配置入口
  */
+@Configuration
 public class DynamicThreadPoolAutoConfig {
 
-    public void dynamicThreadPoolService() {
+    private final Logger logger = LoggerFactory.getLogger(DynamicThreadPoolAutoConfig.class);
 
+    @Bean("dynamicThreadPoolService")
+    public String dynamicThreadPoolService(ApplicationContext applicationContext, Map<String, ThreadPoolExecutor> threadPoolExecutorMap) {
+        String applicationName  = applicationContext.getEnvironment().getProperty("spring.application.name");
+
+        if (StringUtils.isBlank(applicationName)) {
+            applicationName = "缺省的";
+            logger.warn("动态线程池，启动提示。SpringBoot应用未配置spring.application.name 无法获取应用名称！");
+        }
+
+        logger.info("线程池信息：{}", JSON.toJSONString(threadPoolExecutorMap.keySet()));
+
+        return new String();
     }
 }
---
+++ dynamic-thread-pool-test/src/test/java/com/codeagles/test/ApiTest.java
@@ -0,0 +1,19 @@
+package com.codeagles.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.redisson.api.RTopic;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiTest {
+
+
+}
