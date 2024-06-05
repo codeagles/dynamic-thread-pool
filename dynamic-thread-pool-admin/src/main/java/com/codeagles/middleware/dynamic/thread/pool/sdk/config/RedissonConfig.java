807450b1f372e9818ee41504251874a732a06dd0
codeagles <hcn0521@outlook.com>
2024-06-05 23:14:10 +0800

    feat: 增加admin工程

---
+++ dynamic-thread-pool-admin/src/main/java/com/codeagles/middleware/dynamic/thread/pool/sdk/config/RedissonConfig.java
@@ -0,0 +1,44 @@
+package com.codeagles.middleware.dynamic.thread.pool.sdk.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.codec.JsonJacksonCodec;
+import org.redisson.config.Config;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author codeagles
+ * @date 2024/6/5
+ * @description
+ */
+@Configuration
+@EnableConfigurationProperties(RedisClientProperties.class)
+public class RedissonConfig {
+
+    @Bean("redissonClient")
+    public RedissonClient redissonClient(ConfigurableApplicationContext applicationContext, RedisClientProperties properties) {
+        Config config = new Config();
+        // 根据需要可以设定编解码器；https://github.com/redisson/redisson/wiki/4.-%E6%95%B0%E6%8D%AE%E5%BA%8F%E5%88%97%E5%8C%96
+        config.setCodec(JsonJacksonCodec.INSTANCE);
+
+        config.useSingleServer()
+                .setAddress("redis://" + properties.getHost() + ":" + properties.getPort())
+                .setPassword(properties.getPassword())
+                .setConnectionPoolSize(properties.getPoolSize())
+                .setConnectionMinimumIdleSize(properties.getMinIdleSize())
+                .setIdleConnectionTimeout(properties.getIdleTimeout())
+                .setConnectTimeout(properties.getConnectTimeout())
+                .setRetryAttempts(properties.getRetryAttempts())
+                .setRetryInterval(properties.getRetryInterval())
+                .setPingConnectionInterval(properties.getPingInterval())
+                .setKeepAlive(properties.isKeepAlive())
+        ;
+
+        RedissonClient redissonClient = Redisson.create(config);
+
+        return redissonClient;
+    }
+}
